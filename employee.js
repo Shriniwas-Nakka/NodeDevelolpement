164fda1f8392ca65a16f4092b719b9a3379b5968
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-12-26 10:51:16 +0530

    ADDED: functionality to check employee is present or not

---
+++ employee.js
@@ -0,0 +1,8 @@
+let isPresent = 1;
+let employeeCheck = Math.floor(Math.random() * 10) % 2;
+
+if (isPresent == employeeCheck) {
+	console.log("Employee Present!");
+} else {
+	console.log("Employee Absent!");
+}
