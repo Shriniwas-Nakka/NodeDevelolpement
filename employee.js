e181059a325ef41c5128f56ed46902b65b3421b1
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-12-26 11:14:43 +0530

    ADDED: functionality to calculate employee wage

--- employee.js
+++ employee.js
@@ -1,8 +1,14 @@
+let EMP_RATE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empWage = 0;
 let isPresent = 1;
+
 let employeeCheck = Math.floor(Math.random() * 10) % 2;
 
-if (isPresent == employeeCheck) {
-	console.log("Employee Present!");
-} else {
-	console.log("Employee Absent!");
-}
+if (isPresent == employeeCheck)
+	empHrs =  8;
+else
+	empHrs = 0;
+
+empWage = empHrs * EMP_RATE_PER_HOUR;
