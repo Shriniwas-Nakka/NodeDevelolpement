70e8ff37aa763b05f977a4af43f603a0491d994e
Shriniwas-Nakka <nakkashriniwas1@gmail.com>
2019-12-26 17:51:24 +0530

    ADDED: for loop is added to calculate wage of month

--- employee.js
+++ employee.js
@@ -1,21 +1,24 @@
 let IS_PART_TIME = 1;
 let IS_FULL_TIME = 2;
 let EMP_RATE_PER_HOUR = 20;
+let NUM_OF_WORKING_DAYS = 2;
 
 let empHrs = 0;
 let empWage = 0;
+let totalEmpWage = 0;
 
-let employeeCheck = Math.floor(Math.random() * 10) % 3;
-
-switch(employeeCheck) {
-	case IS_PART_TIME:
-		empHrs = 4;
-		break;
-	case IS_FULL_TIME:
-		empHrs = 8;
-		break;
-	default:
-		empHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+	let employeeCheck = Math.floor(Math.random() * 10) % 3;
+	switch(employeeCheck) {
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+	}
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	totalEmpWage += empWage;
 }
-
-empWage = empHrs * EMP_RATE_PER_HOUR;
